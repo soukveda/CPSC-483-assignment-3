8d3551c49e60e232f1149ddf2ba00c6fb6e6c508
Aaron Cope <apc42190@csu.fullerton.edu>
2022-11-19 13:42:25 -0800

    Finished

--- knn.py
+++ knn.py
@@ -1,10 +1,12 @@
 import re
+import math
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
     
 df = pd.read_csv('emails.csv')
@@ -19,10 +21,31 @@ count_vectorizer.fit(X)
 X_train_count = count_vectorizer.transform(X_train)
 X_test_count =  count_vectorizer.transform(X_test)
 
-for n in range(1, 7, 2):
-    knn = KNeighborsClassifier(n_neighbors = n)
-    knn.fit(X_train_count, y_train)
-    y_predictions = knn.predict(X_test_count)
 
-    print(f'Accuracy Score: {metrics.accuracy_score(y_test,y_predictions) * 100}')
-    print(f'Confusion Matrix: {metrics.confusion_matrix(y_test,y_predictions)}')
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train_count, y_train)
+y_predictions = knn.predict(X_test_count)
+
+accuracy = metrics.accuracy_score(y_test,y_predictions)
+error = 1 - accuracy
+interval = interval = 1.96 * math.sqrt( (error * (1 - error)) / X_test_count.shape[0])
+
+print(f'Accuracy Score: {accuracy * 100}')
+print(f'Confusion Matrix:\n{metrics.confusion_matrix(y_test,y_predictions)}')
+print(f'Confidence Interval: {interval}')
+
+
+y_scores = knn.predict_proba(X_test_count)
+fpr, tpr, threshold = metrics.roc_curve(y_test, y_scores[:, 1])
+roc_auc = metrics.auc(fpr, tpr)
+
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.title('ROC Curve of kNN')
+plt.show()
